21d326c339253100917e973cc0a82cd1f23e7f02
bhupendrasambare <bhupendrasam1404@gmail.com>
2024-08-27 00:54:37 +0530

    Dynamic projects

--- src/Projects/BlockchainTodo.jsx
+++ src/Projects/BlockchainTodo.jsx
@@ -4,52 +4,81 @@ import "./style/Blockchiantodo.css"
 import BCT_IMAGE from "../assets/todo.jpg"
 import { BiGlobe, BiLogoGithub, BiLogoReact, BiSolidComponent, BiSolidHardHat } from 'react-icons/bi'
 import { Slide } from 'react-awesome-reveal'
+import coursemangement from "./../assets/course-management.avif"
+import blogImage from "./../assets/blog-img.avif"
+import RestApi from "./../assets/rest-api.jpg"
+import cryptoTracker from "./../assets/crypto-tracker.jpeg"
+import Todo from "./../assets/todo.jpg"
+import Donation from "./../assets/donation.jpg"
+import Weather from "./../assets/weather.png"
+import Movies from "./../assets/movies.png"
+import Microservices from "./../assets/microservices.png"
 
-const BlockchainTodo = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+const images = {
+    coursemangement,
+    blogImage,
+    RestApi,
+    blogImage,
+    Todo,
+    Donation,
+    cryptoTracker,
+    Weather,
+    Movies,
+    Microservices
+  };
+
+const BlockchainTodo = ({data}) => {
 
     return (
         <Motion>
             <section className="portfolio section" id="portfolio">
                 <div className="bct_flex-box">
                     <div className="bct_intro">
-                        <Slide direction='left' triggerOnce cascade  damping={0.3}>
+                        <Slide direction='left' triggerOnce cascade  damping={0.1}>
                             <div className="bct_title">
-                                Blockchain TODO Application
+                                {data.name}
                             </div>
                             <div className="bct_description">
-                                Blockchain Todo DApp is a decentralized application
-                                for storing retrieving and deleting your
-                                daily task list making your day easy
+                                {data.description}
                             </div>
                             <div className="bct_tech_stack">
-                                <BiSolidComponent className='bct_tech_stack_logo' title='Blockchain'/>
+                                {Object.entries(data.tech).map(([techName, iconClass]) => (
+                                    <>
+                                        <div className="">
+                                            <div>
+                                                <i className={iconClass+" remove_extras"} title={techName} key={techName}></i>
+                                            </div>
+                                        </div>
+                                    </>
+                                ))}
+                                {/* <BiSolidComponent className='bct_tech_stack_logo' title='Blockchain'/>
                                 <BiSolidHardHat className='bct_tech_stack_logo' title='Hardhat'/>
                                 <BiLogoReact className='bct_tech_stack_logo' title='React Js'/>
-                                <BiLogoGithub className='bct_tech_stack_logo' title='Github'/>
+                                <BiLogoGithub className='bct_tech_stack_logo' title='Github'/> */}
                             </div>
                             <div className="btc_button_section">
-                                <a className="bct_btn_github" href='https://github.com/bhupendrasambare/Blockchain-TODO'>
+                                <a className="bct_btn_github" target='_blank' href={data.link}>
                                     <BiLogoGithub className='bct_BiLogoGithub'/>
                                     <div className="bct_btn_desc">
                                         Github
                                     </div>
                                 </a>
-                                <a className="bct_btn_Website" href='https://github.com/bhupendrasambare/Blockchain-TODO'>
-                                    <BiGlobe className='bct_BiGlobe'/>
-                                    <div className="bct_btn_desc">
-                                        Github
-                                    </div>
-                                </a>
+                                {
+                                    data.live && 
+                                    <a className="bct_btn_Website" target='_blank' href={data.git}>
+                                        <BiGlobe className='bct_BiGlobe'/>
+                                        <div className="bct_btn_desc">
+                                            Website
+                                        </div>
+                                    </a>
+                                }
                             </div>
                         </Slide>
                     </div>
                     <div className="bct_image">
                         <Slide direction='up' triggerOnce cascade  damping={0.3}>
                         <div className="bct_image_center">
-                            <img src={BCT_IMAGE} className='bct_image-orignal'/>
+                            <img src={images[data.image]} className='bct_image-orignal'/>
                         </div>
                         </Slide>
                     </div>
